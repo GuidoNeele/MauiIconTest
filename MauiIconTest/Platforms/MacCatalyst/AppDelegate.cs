1ad4102d78db3219d7d67f18f429546c6b3c2337
Guido Neele <guido@neele.dev>
2024-04-23 10:05:49 +0200

    Initial commit

---
+++ MauiIconTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace MauiIconTest;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ MauiIconTest/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MauiIconTest.WinUI;
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : MauiWinUIApplication
+{
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        try
+        {
+            InitializeComponent();
+        }
+        catch (XamlParseException parseException)
+        {
+            File.WriteAllText("error.txt", parseException.ToString());
+            throw;
+        }
+        UnhandledException += App_UnhandledException;
+    }
+
+    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    {
+        File.WriteAllText("error.txt", e.Exception.ToString());
+        e.Handled = true;
+    }
+
+    protected override MauiApp CreateMauiApp()
+    {
+        try
+        {
+            return MauiProgram.CreateMauiApp();
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText("error.txt", ex.ToString());
+            throw;
+        }
+    }
+}
+
